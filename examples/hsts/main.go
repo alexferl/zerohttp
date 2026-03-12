f60c4a0a47666151c72702a8c3d6b71ea945f9ee
alexferl <me@alexferl.com>
2026-03-12 11:07:45 -0400

    feat: add HSTS example showing how to enable Strict-Transport-Security
    Signed-off-by: alexferl <me@alexferl.com>

---
+++ examples/hsts/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	zh "github.com/alexferl/zerohttp"
+	"github.com/alexferl/zerohttp/config"
+)
+
+func main() {
+	app := zh.New(
+		config.Config{
+			Addr:     "localhost:8080",
+			TLSAddr:  "localhost:8443",
+			CertFile: "cert.pem",
+			KeyFile:  "key.pem",
+			// Enable HSTS with custom configuration
+			SecurityHeaders: config.SecurityHeadersConfig{
+				StrictTransportSecurity: config.StrictTransportSecurity{
+					MaxAge:            31536000, // 1 year in seconds
+					ExcludeSubdomains: false,    // Apply to subdomains too
+					PreloadEnabled:    false,    // Set to true only if submitting to hstspreload.org
+				},
+			},
+		},
+	)
+
+	app.GET("/", zh.HandlerFunc(func(w http.ResponseWriter, r *http.Request) error {
+		return zh.R.JSON(w, http.StatusOK, zh.M{
+			"message": "HSTS enabled!",
+			"tls":     r.TLS != nil,
+		})
+	}))
+
+	log.Fatal(app.Start())
+}
