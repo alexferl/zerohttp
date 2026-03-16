a55e2b4fe3e9b3ed8c2ac0c8db439c36033933ad
alexferl <me@alexferl.com>
2026-03-16 17:30:50 -0400

    docs: reorganize README and simplify go.mod version
    Signed-off-by: alexferl <me@alexferl.com>

--- examples/core/hello_world/main.go
+++ examples/core/hello_world/main.go
@@ -10,8 +10,9 @@ import (
 func main() {
 	app := zh.New()
 
-	app.GET("/", zh.HandlerFunc(func(w http.ResponseWriter, r *http.Request) error {
-		return zh.R.JSON(w, 200, zh.M{"message": "Hello, World!"})
+	app.GET("/hello/{name}", zh.HandlerFunc(func(w http.ResponseWriter, r *http.Request) error {
+		name := zh.Param(r, "name")
+		return zh.Render.JSON(w, http.StatusOK, zh.M{"message": "Hello, " + name + "!"})
 	}))
 
 	log.Fatal(app.Start())
